1cd172cae3b327e0211185aaab4da032e5bdcc63
joom <113173627+joom122796@users.noreply.github.com>
2023-01-26 14:05:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from alphabet import alph as alph
 from alphabet import list as list
-m = []
-def strToMorse(str):
+from alphabet import m as m
+
+def morseEncode(str):
+  str = str.lower()
   for x in str:
     if x in alph:
       pos = alph.index(x)
       morse = list[pos]
-      m.extend(morse)
-  string2 = ''
-  print(string2.join(m))
+      m.extend(morse+' ')
+  encode = ''.join(m)
+  print(encode)
 
 string = input('Enter string to convert to morse code: ')
-string = string.lower()
-strToMorse(string)
+morseEncode(string)
